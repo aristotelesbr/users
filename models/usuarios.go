e3f89232db841f7cc6c91a1226897c214378ac2e
Aristoteles <aristotelesbr2014@gmail.com>
2019-06-09 08:34:33 -0300

    added call to connect databa in usuarios model

--- models/usuarios.go
+++ models/usuarios.go
@@ -1,8 +1,13 @@
 package models
 
+import "usuarios/lib"
+
 // Usuarios database table
 type Usuarios struct {
 	ID    int    `db:"id" json:"id"`
 	Nome  string `db:"nome" json:"nome"`
 	Email string `sb:"email" json:"email"`
 }
+
+// UsuarioModel receiver connection config to connect database
+var UsuarioModel = lib.Sess.Collection("usuarios")
